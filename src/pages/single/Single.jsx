de416a2e5c51abe51dbf9b0333c7c19b42aea2d4
mirakov <holturaevm@gmail.com>
2023-05-01 22:43:35 +0500

    some changes, finish single component

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -4,6 +4,7 @@ import './single.scss'
 import Sidebar from './../../components/sidebar/Sidebar';
 import Navbar from './../../components/navbar/Navbar';
 import Chart from './../../components/chart/Chart';
+import ProductTable from '../../components/table/Table';
 
 const Single = () => {
   return (
@@ -47,6 +48,8 @@ const Single = () => {
           </div>
         </div>
         <div className="bottom">
+        <h1 className="title">Last Transactions</h1>
+          <ProductTable />
         </div>
       </div>
     </div>
