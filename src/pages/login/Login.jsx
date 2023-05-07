bc2d6e72049d2796a397ce186580affcb68c6542
mirakov <holturaevm@gmail.com>
2023-05-07 16:47:23 +0500

    add fake account to the login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,26 @@
 import { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import CopyToClipboard from "reactjs-copy-to-clipboard";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 import './login.scss'
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../firebase';
-import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './../../context/AuthContext';
+import copyIcon from '../../images/copy-icon.png'
 
 const Login = () => {
   const [error, setError] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [statusLogin, setStatusLogin] = useState({
+    copied: false,
+    allowCopy: false,
+  });
+  const [statusPassword, setStatusPassword] = useState({
+    copied: false,
+    allowCopy: false,
+  });
+
 
   const navigate = useNavigate()
 
@@ -24,35 +35,86 @@ const Login = () => {
         const user = userCredential.user;
         dispatch({ type: "LOGIN", payload: user })
         navigate('/')
-        console.log("🚀 ~ file: Login.jsx:19 ~ .then ~ user:", user)
-
       })
       .catch((error) => {
-        console.log("🚀 ~ file: Login.jsx:23 ~ handleLogin ~ error:", error)
         setError(error)
       });
+  }
 
+  const copyLogin = () => {
+    setStatusLogin({ copied: true, allowCopy: true });
+    setTimeout(() => {
+      setStatusLogin({ copied: false, allowCopy: false });
+    }, 3000)
+  }
+
+  const copyPassword = () => {
+    setStatusPassword({ copied: true, allowCopy: true });
+    setTimeout(() => {
+      setStatusPassword({ copied: false, allowCopy: false });
+    }, 3000)
   }
 
   return (
-    <div className='login'>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder='email'
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder='password'
-          onChange={e => setPassword(e.target.value)}
-        />
-        <button type='submit'>Login</button>
-        {error &&
-          <span>{error.message}</span>
-        }
+    <div className="loginMain">
+      <div className='login'>
+        <div className='loginTxt'>
+          <h4>Here's an account to log in...</h4>
+          <section>
+            <div className='copyRow'>
+              <span>Email: </span>
+              <b> <code>example_email@gmail.com</code></b>
+              <button
+                title='copy'
+                onClick={copyLogin}
+                style={{
+                  display: statusLogin.allowCopy
+                    ? "none"
+                    : 'block'
+                }}>
+                <CopyToClipboard text={'example_email@gmail.com'}>
+                  <img src={copyIcon} alt="copy" />
+                </CopyToClipboard>
+              </button>
+              {statusLogin.copied && <code className='status'>Copied!</code>}
+            </div>
+            <div className='copyRow'>
+              <span>Password: </span>
+              <b> <code>example_password</code></b>
+              <button
+                title='copy'
+                onClick={copyPassword}
+                style={{
+                  display: statusPassword.allowCopy
+                    ? "none"
+                    : 'block'
+                }}>
+                <CopyToClipboard text={'example_password'}>
+                  <img src={copyIcon} alt="copy" />
+                </CopyToClipboard>
+              </button>
+              {statusPassword.copied && <code className='status'>Copied!</code>}
+            </div>
+          </section>
+        </div>
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder='email'
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder='password'
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type='submit'>Login</button>
+          {error &&
+            <span>{error.message}</span>
+          }
 
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
